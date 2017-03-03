5dc1c5de4bb6c82df222ea55cb7e338985eed7cc
BigEgg <cd_bigegg@hotmail.com>
2017-03-04 00:34:47 +0800

    Add a validation attribute for required if check

---
+++ BigEgg.Core.Tests/Validations/RequiredIfAttributeTest.cs
@@ -0,0 +1,223 @@
+﻿using BigEgg.Validations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigEgg.Tests.Validations
+{
+    public class RequiredIfAttributeTest
+    {
+        [TestClass]
+        public class GeneralValidationTest
+        {
+            [TestMethod]
+            public void ValidationTest_NotMatch()
+            {
+                Person person = new Person();
+
+                var validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+            }
+
+            [TestMethod]
+            public void ValidationTest_Match()
+            {
+                Person person = new Person()
+                {
+                    IsCouple = true
+                };
+
+                var validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+
+                person.CoupleName = "";
+                validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+
+                person.CoupleName = "   ";
+                validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+
+                person.CoupleName = "BigEgg";
+                validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+            }
+
+            [TestMethod]
+            public void ValidationTest_NotMatch_OtherType()
+            {
+                Person2 person = new Person2()
+                {
+                    IsCouple = "No"
+                };
+
+                var validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+            }
+
+            [TestMethod]
+            public void ValidationTest_Match_OtherType()
+            {
+                Person2 person = new Person2()
+                {
+                    IsCouple = "Yes"
+                };
+
+                var validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ValidationException))]
+            public void ValidationTest_DependentPropertyNotExist()
+            {
+                WrongPerson person = new WrongPerson()
+                {
+                    IsCouple = true
+                };
+
+                person.Validate();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ValidationException))]
+            public void ValidationTest_DependentPropertyTypeNotMatch()
+            {
+                WrongPerson2 person = new WrongPerson2()
+                {
+                    IsCouple = true
+                };
+
+                person.Validate();
+            }
+        }
+
+        [TestClass]
+        public class IsInvertedTest
+        {
+            [TestMethod]
+            public void ValidationTest_NotMatch()
+            {
+                PersonWithInverted person = new PersonWithInverted()
+                {
+                    IsNotCouple = true
+                };
+
+                var validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+            }
+
+            [TestMethod]
+            public void ValidationTest_Match()
+            {
+                PersonWithInverted person = new PersonWithInverted();
+
+                var validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.AreEqual(Person.CoupleNameErrorMessage, validateResult.Single().ErrorMessage);
+            }
+        }
+
+        [TestClass]
+        public class AllowEmptyStringTest
+        {
+            [TestMethod]
+            public void ValidationTest_NotMatch()
+            {
+                PersonWithAllowEmpty person = new PersonWithAllowEmpty();
+
+                var validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+            }
+
+            [TestMethod]
+            public void ValidationTest_Match()
+            {
+                PersonWithAllowEmpty person = new PersonWithAllowEmpty()
+                {
+                    IsCouple = true
+                };
+
+                var validateResult = person.Validate();
+                Assert.AreEqual(1, validateResult.Count());
+                Assert.IsFalse(string.IsNullOrWhiteSpace(validateResult.Single().ErrorMessage));
+
+                person.CoupleName = "";
+                validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+
+                person.CoupleName = "   ";
+                validateResult = person.Validate();
+                Assert.IsFalse(validateResult.Any());
+            }
+        }
+
+
+        private class Person : ValidatableObject
+        {
+            public const string CoupleNameErrorMessage = "Couple's name is mandatory.";
+
+            public bool IsCouple { get; set; }
+
+            [RequiredIf("IsCouple", true, ErrorMessage = CoupleNameErrorMessage)]
+            public string CoupleName { get; set; }
+        }
+
+        private class Person2 : ValidatableObject
+        {
+            public const string CoupleNameErrorMessage = "Couple's name is mandatory.";
+
+            public string IsCouple { get; set; }
+
+            [RequiredIf("IsCouple", "Yes", ErrorMessage = CoupleNameErrorMessage)]
+            public string CoupleName { get; set; }
+        }
+
+        private class WrongPerson : ValidatableObject
+        {
+            public const string CoupleNameErrorMessage = "Couple's name is mandatory.";
+
+            public bool IsCouple { get; set; }
+
+            [RequiredIf("WrongPropertyName", true, ErrorMessage = CoupleNameErrorMessage)]
+            public string CoupleName { get; set; }
+        }
+
+        private class WrongPerson2 : ValidatableObject
+        {
+            public const string CoupleNameErrorMessage = "Couple's name is mandatory.";
+
+            public bool IsCouple { get; set; }
+
+            [RequiredIf("IsCouple", "Yes", ErrorMessage = CoupleNameErrorMessage)]
+            public string CoupleName { get; set; }
+        }
+
+        private class PersonWithInverted : ValidatableObject
+        {
+            public const string CoupleNameErrorMessage = "Couple's name is mandatory.";
+
+            public bool IsNotCouple { get; set; }
+
+            [RequiredIf("IsNotCouple", true, ErrorMessage = CoupleNameErrorMessage, IsInverted = true)]
+            public string CoupleName { get; set; }
+        }
+
+        private class PersonWithAllowEmpty : ValidatableObject
+        {
+            public bool IsCouple { get; set; }
+
+            [RequiredIf("IsCouple", true, AllowEmptyStrings = true)]
+            public string CoupleName { get; set; }
+        }
+    }
+}
---
+++ BigEgg.Core/Validations/RequiredIfAttribute.cs
@@ -0,0 +1,79 @@
+﻿namespace BigEgg.Validations
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Reflection;
+
+    /// <summary>
+    /// Specifies that a data field value is required when another property's value is equal to some specific data.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        private string dependentPropertyName;
+        private object matchValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiredIfAttribute" /> class with the specified property name
+        /// and the value which should be equal to.
+        /// </summary>
+        /// <param name="dependentPropertyName">The specified property name which need to be compared.</param>
+        /// <param name="matchValue">The specified value to compare.</param>
+        public RequiredIfAttribute(string dependentPropertyName, object matchValue)
+        {
+            Preconditions.NotNullOrWhiteSpace(dependentPropertyName, "dependentPropertyNamw");
+            Preconditions.NotNull(matchValue, "matchValue");
+
+            this.dependentPropertyName = dependentPropertyName;
+            this.matchValue = matchValue;
+        }
+
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether an empty string is allowed.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if an empty string is allowed; otherwise, <c>false</c>. The default value is false.
+        /// </value>
+        public bool AllowEmptyStrings { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether dependent property's value should match or differ from the match value.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if dependent property's value validation should be inverted; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsInverted { get; set; }
+
+
+        /// <summary>
+        /// Validates the specified value with respect to the current validation attribute.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <param name="validationContext">The context information about the validation operation.</param>
+        /// <returns>An instance of the <see cref="ValidationResult"/> class.</returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Preconditions.NotNull(validationContext, "validationContext");
+
+            PropertyInfo dependentProperty = validationContext.ObjectType.GetProperty(dependentPropertyName);
+            if (dependentProperty == null) { throw new ValidationException($"Cannot find the dependent property '{dependentPropertyName}'."); }
+
+            var dependentPropertyValue = dependentProperty.GetValue(validationContext.ObjectInstance);
+            if (dependentPropertyValue.GetType() != matchValue.GetType()) { throw new ValidationException($"The dependent property '{dependentPropertyName}' ({matchValue.GetType().Name}) has different type with the match value ({matchValue.GetType().Name})."); }
+
+            if (!IsInverted && Equals(dependentPropertyValue, matchValue) ||
+                IsInverted && !Equals(dependentPropertyValue, matchValue))
+            {
+                if ((value == null) ||
+                    (value is string && (string.IsNullOrWhiteSpace(value as string) && !AllowEmptyStrings)))
+                {
+                    return IsInverted
+                        ? new ValidationResult(ErrorMessage ?? $"'{validationContext.DisplayName}' is mandatory because value of '{dependentPropertyName}' not match '{matchValue}'.")
+                        : new ValidationResult(ErrorMessage ?? $"'{validationContext.DisplayName}' is mandatory because value of '{dependentPropertyName}' matches '{matchValue}'.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
