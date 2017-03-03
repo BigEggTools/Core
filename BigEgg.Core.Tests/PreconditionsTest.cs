c84a71e3f7ce9269ca6a6abf41311415e54331e2
BigEgg <cd_bigegg@hotmail.com>
2017-03-03 22:43:26 +0800

    Add the preconditions logic and their tests

---
+++ BigEgg.Core.Tests/PreconditionsTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BigEgg.Tests
+{
+    public class PreconditionsTest
+    {
+        [TestClass]
+        public class NotNullTest
+        {
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void Null()
+            {
+                Preconditions.NotNull(null);
+            }
+
+            [TestMethod]
+            public void NotNull()
+            {
+                Preconditions.NotNull("");
+            }
+
+            [TestMethod]
+            public void Message()
+            {
+                try
+                {
+                    Preconditions.NotNull(null, "param");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Assert.IsTrue(ex.Message.Contains("'param'"));
+                    Assert.IsTrue(ex.Message.Contains("'Message'"));
+                }
+            }
+        }
+
+        [TestClass]
+        public class NotNullOrWhiteSpaceTest
+        {
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void Null()
+            {
+                Preconditions.NotNullOrWhiteSpace(null);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void Empty()
+            {
+                Preconditions.NotNullOrWhiteSpace("");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void WhiteSpace()
+            {
+                Preconditions.NotNullOrWhiteSpace("  ");
+            }
+
+            [TestMethod]
+            public void ValidValue()
+            {
+                Preconditions.NotNullOrWhiteSpace("abc");
+            }
+
+            [TestMethod]
+            public void Message()
+            {
+                try
+                {
+                    Preconditions.NotNullOrWhiteSpace(null, "param");
+                }
+                catch (ArgumentException ex)
+                {
+                    Assert.IsTrue(ex.Message.Contains("'param'"));
+                    Assert.IsTrue(ex.Message.Contains("'Message'"));
+                }
+            }
+        }
+
+        [TestClass]
+        public class CheckTest
+        {
+            [TestMethod]
+            public void Match()
+            {
+                Preconditions.Check(string.IsNullOrWhiteSpace(null));
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void NotMath()
+            {
+                Preconditions.Check(!string.IsNullOrWhiteSpace(null));
+            }
+
+            [TestMethod]
+            public void Message()
+            {
+                try
+                {
+                    Preconditions.Check(!string.IsNullOrWhiteSpace(null), "Parameter '{0}' in '{1}' should not be empty string.", "param");
+                }
+                catch (ArgumentException ex)
+                {
+                    Assert.IsTrue(ex.Message.Contains("'param'"));
+                    Assert.IsTrue(ex.Message.Contains("'Message'"));
+                }
+            }
+        }
+
+        [TestClass]
+        public class GenericCheckTest
+        {
+            [TestMethod]
+            public void Match()
+            {
+                Preconditions.Check<NotSupportedException>(string.IsNullOrWhiteSpace(null));
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(NotSupportedException))]
+            public void NotMath()
+            {
+                Preconditions.Check<NotSupportedException>(!string.IsNullOrWhiteSpace(null));
+            }
+
+            [TestMethod]
+            public void Message()
+            {
+                try
+                {
+                    Preconditions.Check<NotSupportedException>(!string.IsNullOrWhiteSpace(null), "Parameter '{0}' in '{1}' should not be empty string.", "param");
+                }
+                catch (NotSupportedException ex)
+                {
+                    Assert.IsTrue(ex.Message.Contains("'param'"));
+                    Assert.IsTrue(ex.Message.Contains("'Message'"));
+                }
+            }
+        }
+    }
+}
---
+++ BigEgg.Core/Preconditions.cs
@@ -0,0 +1,74 @@
+﻿namespace BigEgg
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Hold all the precondition logics.
+    /// </summary>
+    public class Preconditions
+    {
+        /// <summary>
+        /// Throw an <see cref="ArgumentNullException" /> if the object is null.
+        /// </summary>
+        /// <param name="obj">The object which need to check.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="methodName">Name of the method.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void NotNull(object obj, string paramName = "", [CallerMemberName] string methodName = "")
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException($"Parameter '{paramName}' in '{methodName}' should not be null.");
+            }
+        }
+
+        /// <summary>
+        /// Throw an <see cref="ArgumentException" /> if a specified string is null,
+        /// empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="methodName">Name of the method.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void NotNullOrWhiteSpace(string value, string paramName = "", [CallerMemberName] string methodName = "")
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"Parameter '{paramName}' in '{methodName}' should not be empty string.");
+            }
+        }
+
+        /// <summary>
+        /// Throw an <see cref="ArgumentException" /> if the condition is false.
+        /// </summary>
+        /// <param name="condition">The check condition.</param>
+        /// <param name="messageFormat">The error message format.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="methodName">Name of the method.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Check(bool condition, string messageFormat = "", string paramName = "", [CallerMemberName] string methodName = "")
+        {
+            if (!condition)
+            {
+                throw new ArgumentException(string.Format(messageFormat, paramName, methodName));
+            }
+        }
+
+        /// <summary>
+        /// Throw an <see cref="ArgumentNullException" /> if the condition is false.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception should thrown.</typeparam>
+        /// <param name="condition">The check condition.</param>
+        /// <param name="messageFormat">The error message format.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="methodName">Name of the method.</param>
+        public static void Check<TException>(bool condition, string messageFormat = "", string paramName = "", [CallerMemberName] string methodName = "") where TException : Exception
+        {
+            if (!condition)
+            {
+                throw (TException)Activator.CreateInstance(typeof(TException), new object[] { string.Format(messageFormat, paramName, methodName) });
+            }
+        }
+    }
+}
